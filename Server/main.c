9ab1c540bf5e6c2295f2b6e8f786ae805825ee23
Matthew Dahl © <matthewdahl@Matthew-Dahls-MacBook-Pro.local>
2012-02-05 13:00:19 -0500

    Initial Commit

---
+++ Server/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Server
+//
+//  Created by Matthew Dahl on 2/5/12.
+//  Copyright 2012 Mintruss, LLC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
